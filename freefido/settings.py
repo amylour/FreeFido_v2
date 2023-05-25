348a149dd37a4ca5fed291703c406e002a25a882
AMY RICHARDSON <amyloumc@gmail.com>
2023-05-25 22:05:48 +0000

    add:index.html and base template

--- freefido/settings.py
+++ freefido/settings.py
@@ -64,7 +64,9 @@ ROOT_URLCONF = 'freefido.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
