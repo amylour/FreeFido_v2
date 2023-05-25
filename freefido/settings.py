216e69ed29200653b42d5565911f4f7bb232bd48
AMY RICHARDSON <amyloumc@gmail.com>
2023-05-25 21:50:00 +0000

    fix:templates DIR typo

--- freefido/settings.py
+++ freefido/settings.py
@@ -9,18 +9,18 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os
+
 from pathlib import Path
+import os
 import dj_database_url
+if os.path.exists('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
-
-if os.path.exists('env.py'):
-    import env
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -31,8 +31,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '8000-amylour-freefido-v2-6ev1mnumgv.us2.codeanyapp.com', 'freefido.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-amylour-freefido-v2-6ev1mnumgv.us2.codeanyapp.com',
+                 'freefido.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -64,8 +64,7 @@ ROOT_URLCONF = 'freefido.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['DIRS': [TEMPLATES_DIR],
-                 ],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
