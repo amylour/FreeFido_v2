083bbbe5a2496571e1a5c5c199ef1ef24fdeecca
AMY RICHARDSON <amyloumc@gmail.com>
2023-05-26 20:27:54 +0000

    add:bootstrap crispy forms

--- freefido/settings.py
+++ freefido/settings.py
@@ -46,12 +46,14 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
     'cloudinary',
     'home',
+    'crispy_forms',
+    'crispy_bootstrap5',
+
 ]
 
 SITE_ID = 1
@@ -65,6 +67,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
 
 ROOT_URLCONF = 'freefido.urls'
 
@@ -83,6 +89,10 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field'
+            ]
         },
     },
 ]
