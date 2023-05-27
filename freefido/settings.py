347a317bc7b7ca1de605e6e7ff17d71ffc9d090b
AMY RICHARDSON <amyloumc@gmail.com>
2023-05-27 13:11:42 +0000

    chore:install django summernote and setup

--- freefido/settings.py
+++ freefido/settings.py
@@ -50,6 +50,8 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'cloudinary',
+    'django_summernote',
+    'articles',
     'home',
     'crispy_forms',
     'crispy_bootstrap5',
--- freefido/urls.py
+++ freefido/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('summernote/', include('django_summernote.urls')),
     path('', include('home.urls')),
 ]
