15eec9e524ef34a64cb4b1c1ebf3854afe822b54
AMY RICHARDSON <amyloumc@gmail.com>
2023-05-27 20:23:26 +0000

    setup:article page view and connect urls

--- articles/urls.py
+++ articles/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.ArticleList.as_view(), name='articles')
+    path('', views.ArticleList.as_view(), name='articles'),
+    path('<slug:slug>/', views.ArticlePage.as_view(), name='article_page')
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Article
 
 
@@ -8,3 +8,25 @@ class ArticleList(generic.ListView):
     queryset = Article.objects.filter(status=1).order_by('-created_on')
     template_name = 'articles/articles.html'
     paginate_by = 6
+
+
+class ArticlePage(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Article.objects.filter(status=1)
+        article = get_object_or_404(queryset, slug=slug)
+        comments = article.comments.filter(
+            approved=True).order_by('created_on')
+        liked = False
+        if article.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            'articles/article_page.html',
+            {
+                'article': article,
+                'comments': comments,
+                'liked': liked
+            },
+        )
