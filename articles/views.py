601c6a436a4cf94a46ad2533c2956fe264addb46
AMY RICHARDSON <amyloumc@gmail.com>
2023-05-27 20:47:17 +0000

    add:CommentForm to ArticlePage

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Article
+from .forms import CommentForm
 
 
 class ArticleList(generic.ListView):
@@ -27,6 +28,7 @@ class ArticlePage(View):
             {
                 'article': article,
                 'comments': comments,
-                'liked': liked
+                'liked': liked,
+                'comment_form': CommentForm()
             },
         )
