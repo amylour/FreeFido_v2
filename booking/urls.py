351d191ddf3f9ba68412073d34116e399f53de77
AMY RICHARDSON <amyloumc@gmail.com>
2023-06-08 17:17:23 +0000

    edit: booking saving and displaying

--- booking/urls.py
+++ booking/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('create/', views.CreateBooking.as_view(), name='booking_create'),
+    path('success/',views.ActiveBookings.as_view(), name='booking_success'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.views import generic, View
 from django.views.generic import CreateView, ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Booking
 from .forms import BookingForm
+
+from django.urls import reverse_lazy
 
 
 class CreateBooking(LoginRequiredMixin, CreateView):
@@ -12,9 +15,29 @@ class CreateBooking(LoginRequiredMixin, CreateView):
     model = Booking
     form_class = BookingForm
     template_name = 'booking/booking_create.html'
-    success_url = '/booking/'
+    # Use the URL name of your booking success page
+    success_url = reverse_lazy('booking_success')
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+
+class ActiveBookings(LoginRequiredMixin, generic.ListView):
+    """
+    A view to display the user's bookings
+    """
+    model = Booking
+    template_name = 'booking/booking_success.html'
+    context_object_name = 'bookings'
+
+    def get_queryset(self):
+        # Filter bookings based on the currently logged-in user
+        return Booking.objects.filter(user=self.request.user).order_by('-date')
+
+    
+
+
+
+
 
