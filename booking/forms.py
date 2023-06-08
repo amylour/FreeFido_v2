ff378b9d478e48f3f6a08108c1525c80a7c57cb6
AMY RICHARDSON <amyloumc@gmail.com>
2023-06-08 18:22:07 +0000

    edit: alert to unavailable booking

--- booking/forms.py
+++ booking/forms.py
@@ -16,7 +16,20 @@ class BookingForm(forms.ModelForm):
 
         self.fields['date'].widget = forms.DateInput(attrs={'type': 'date'})
 
+        def form_valid(self, form):
+            form.instance.user = self.request.user
+
+            # Check if a booking with the same date and time already exists
+            if Booking.objects.filter(date=form.instance.date, time=form.instance.time).exists():
+                messages.error(
+                    self.request, 'This date and time is already booked.')
+                return self.form_invalid(form)
+
+            messages.success(self.request, 'Your booking has been saved.')
+            return super().form_valid(form)
+
     class Meta:
         model = Booking
         fields = ['first_name', 'last_name', 'email', 'dog_name', 'breed', 'color',
                   'is_vaccinated', 'gender', 'date', 'time']
+        unique_together = ['date', 'time']
