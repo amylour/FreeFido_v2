150e690aa0f4e423d55f111de974a722b7fded41
AMY RICHARDSON <amyloumc@gmail.com>
2023-06-20 16:21:13 +0000

    test: deployment commit, changed DEBUG->False

--- freefido/settings.py
+++ freefido/settings.py
@@ -30,7 +30,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['8000-amylour-freefido-v2-6ev1mnumgv.us2.codeanyapp.com',
                  'freefido.herokuapp.com', 'localhost']
