c0735c48a995259b46ce887479379aa87238c2f9
AMY RICHARDSON <amyloumc@gmail.com>
2023-06-23 14:32:14 +0000

    chore: remove delete profile url

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,6 +4,5 @@ from .views import ProfileView, ProfileEdit, ProfileDelete
 urlpatterns = [
     path('user/<int:pk>/', ProfileView.as_view(), name='profile'),
     path('edit/<int:pk>/', ProfileEdit.as_view(), name='profile_edit'),
-    path('delete/<int:pk>/', ProfileDelete.as_view(), name='delete_profile')
 ]
 
